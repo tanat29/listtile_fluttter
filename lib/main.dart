74daf2621bff771293295bc3965c680073836aba
Tanat29 <tanat29.mongkon@gmail.com>
2022-12-17 21:46:26 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ListTile Demo123'),
+        title: const Text('ListTile Demo'),
         backgroundColor: Colors.indigo,
       ),
       body: Center(
